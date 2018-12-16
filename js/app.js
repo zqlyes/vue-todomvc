189d4af679110b8861a2a2dc6c3fd4f753e70fc7
zqlyes <1959840503@qq.com>
2018-12-16 22:41:58 +0800

    achieved additions, deletions，changes and rendering data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	 
+	const vm = new Vue({
+		el: '.todoapp',
+		data: {
+			list:[
+				{id:1, name:'zhangsan',completed:true},
+				{id:2, name:'lisi', completed: false},
+				{id:3, name:'jack', completed: false},
+			],
+			todoMsg: '',
+		  clickId: -1
+		},
+		methods:{
+			addTodo(){
+				this.list.unshift({
+					id: +new Date(),
+					name: this.todoMsg,
+					completed: false
+				})
+				this.todoMsg = ''
+			},
+			delTodo(id){
+				let index = this.list.findIndex(item => item.id == id)
+				this.list.splice(index,1);
+			},
+			showEdit(id){
+        this.clickId = id
+			},
+			updateTodo(){
+				this.clickId = -1;
+			},
+			clearTodos(){
+				this.list = this.list.filter(item => !item.completed)
+			}
+		}
 
+	})
+	  
+	
 })(window);
